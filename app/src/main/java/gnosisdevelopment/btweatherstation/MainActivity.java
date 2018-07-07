08c9b9d88f31460cad76bc90da5ff25356e27c15
James Harley <jahsnote5@gmail.com>
2018-07-07 12:51:31 -0400

    bt foundation part 1

--- app/src/main/java/gnosisdevelopment/btweatherstation/MainActivity.java
+++ app/src/main/java/gnosisdevelopment/btweatherstation/MainActivity.java
@@ -1,15 +1,23 @@
 package gnosisdevelopment.btweatherstation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent weatherPanelIntent;
     private TextView mTextMessage;
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private double temp = 27.00;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    //startActivity(weatherPanelIntent);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -35,10 +44,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressStatus = (int)temp;
+        progressBar.setProgress(progressStatus);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        weatherPanelIntent = new Intent(this, WeatherPanel.class);
     }
 
 }
