9a5d4241e40d28de564ee243b96faaf0135fde7e
James Harley <jahsnote5@gmail.com>
2018-07-09 09:01:44 -0400

    bluetooth chat example integrated, before major customization

--- app/src/main/java/gnosisdevelopment/btweatherstation/MainActivity.java
+++ app/src/main/java/gnosisdevelopment/btweatherstation/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -13,6 +14,7 @@ import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton radioF;
     private RadioButton radioC;
 
-    final String DEGREE  = "\u00b0";
-    private Double temp = 27.00;
+    final String DEGREE = "\u00b0";
+    private Double temp = 28.00;
     private Double humidity = 50.10;
     private Double wind = 15.5;
 
@@ -76,35 +78,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Child layout
-        inflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
         weatherPanelInflator();
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         weatherPanelIntent = new Intent(this, WeatherPanel.class);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothChatFragment fragment = new BluetoothChatFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
     }
 
-    protected  void setTemp (){
-        if(celsius==true) {
+    protected void setTemp() {
+        if (celsius == true) {
             tempText.setText(String.valueOf(temp) + DEGREE);
-        }
-        else{
+        } else {
             tempText.setText(String.valueOf(cToF(temp)) + DEGREE);
         }
     }
-    protected  void setHumidity (){
-        humidityText.setText(String.valueOf(humidity) + "%" );
+
+    protected void setHumidity() {
+        humidityText.setText(String.valueOf(humidity) + "%");
     }
-    protected  void setWind (){
-        windText.setText(String.valueOf(wind) );
+
+    protected void setWind() {
+        windText.setText(String.valueOf(wind));
     }
+
     // convert C to F
-    protected Double cToF(Double cTemp){
-        return  (temp * 9/5.0) +32;
+    protected Double cToF(Double cTemp) {
+        return (temp * 9 / 5.0) + 32;
     }
-    protected void weatherPanelInflator(){
+
+    protected void weatherPanelInflator() {
         weatherPanelDeflator();
-        if ( tempState == true){
+        if (tempState == true) {
             tempLayout = inflater.inflate(R.layout.weather_temp,
                     (ViewGroup) findViewById(R.id.weatherPanelTemp));
 
@@ -113,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
 
             tempText = (TextView) findViewById(R.id.temp);
         }
-        if(humidityState == true){
+        if (humidityState == true) {
             humidityLayout = inflater.inflate(R.layout.weather_humidity,
                     (ViewGroup) findViewById(R.id.weatherPanelHumidity));
 
@@ -122,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
 
             humidityText = (TextView) findViewById(R.id.humidity);
         }
-        if(windState == true){
+        if (windState == true) {
             windLayout = inflater.inflate(R.layout.weather_wind,
                     (ViewGroup) findViewById(R.id.weatherPanelWind));
 
@@ -131,46 +143,49 @@ public class MainActivity extends AppCompatActivity {
 
             windText = (TextView) findViewById(R.id.wind);
         }
-        if(temp != null && tempText != null){
+        if (temp != null && tempText != null) {
             setTemp();
         }
-        if (humidity != null && humidityText != null ){
+        if (humidity != null && humidityText != null) {
             setHumidity();
         }
-        if (wind != null && windText != null){
+        if (wind != null && windText != null) {
             setWind();
         }
     }
+
     protected void weatherPanelDeflator() {
-        if(tempLayout != null)weatherView.removeView(tempLayout);
-        if(humidityLayout != null)weatherView.removeView(humidityLayout);
-        if( windLayout != null) weatherView.removeView(windLayout);
+        if (tempLayout != null) weatherView.removeView(tempLayout);
+        if (humidityLayout != null) weatherView.removeView(humidityLayout);
+        if (windLayout != null) weatherView.removeView(windLayout);
     }
-    protected void controlPanelInflator(){
+
+    protected void controlPanelInflator() {
         controlPanelDeflator();
         controlLayout = inflater.inflate(R.layout.control_panel,
                 (ViewGroup) findViewById(R.id.controlPanel));
         weatherView = findViewById(R.id.container);
         weatherView.addView(controlLayout);
 
-        tempSw = (Switch)findViewById(R.id.tempSwitch);
-        humidSw = (Switch)findViewById(R.id.humiditySwitch);
-        windSw  = (Switch)findViewById(R.id.windSwitch);
+        tempSw = (Switch) findViewById(R.id.tempSwitch);
+        humidSw = (Switch) findViewById(R.id.humiditySwitch);
+        windSw = (Switch) findViewById(R.id.windSwitch);
 
         radioC = findViewById(R.id.radio_celsius);
         radioF = findViewById(R.id.radio_fahrenheit);
 
-        if(tempState==true)tempSw.setChecked(true);
-        if(humidityState==true)humidSw.setChecked(true);
-        if(windState==true)windSw.setChecked(true);
+        if (tempState == true) tempSw.setChecked(true);
+        if (humidityState == true) humidSw.setChecked(true);
+        if (windState == true) windSw.setChecked(true);
 
         tempSw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // do something, the isChecked will be
                 // true if the switch is in the On position
-                if(isChecked == false){
+                if (isChecked == false) {
                     tempState = false;
-                }if(isChecked == true){
+                }
+                if (isChecked == true) {
                     tempState = true;
                 }
             }
@@ -179,9 +194,10 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // do something, the isChecked will be
                 // true if the switch is in the On position
-                if(isChecked == false){
+                if (isChecked == false) {
                     humidityState = false;
-                }if(isChecked == true){
+                }
+                if (isChecked == true) {
                     humidityState = true;
                 }
             }
@@ -190,42 +206,45 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // do something, the isChecked will be
                 // true if the switch is in the On position
-                if(isChecked == false){
+                if (isChecked == false) {
                     windState = false;
-                }if(isChecked == true){
+                }
+                if (isChecked == true) {
                     windState = true;
                 }
             }
         });
         radioView = findViewById(R.id.radio_fahrenheit);
-        if(celsius==true){
+        if (celsius == true) {
             radioC.setChecked(true);
-        }
-        else{
+        } else {
             radioF.setChecked(true);
         }
         onRadioButtonClicked(radioView);
 
     }
+
     protected void controlPanelDeflator() {
-        if(controlLayout != null) weatherView.removeView(controlLayout);
+        if (controlLayout != null) weatherView.removeView(controlLayout);
     }
+
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.radio_celsius:
                 if (checked)
                     // Pirates are the best
-                    celsius =true;
-                    break;
+                    celsius = true;
+                break;
             case R.id.radio_fahrenheit:
                 if (checked)
                     // Ninjas rule
                     celsius = false;
-                    break;
+                break;
         }
     }
 }
+
